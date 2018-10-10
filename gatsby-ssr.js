77828570094101f3caf17b590677f8fa5b406f90
John C. Munson <johncmunson91@gmail.com>
2018-10-10 12:15:20 -0500

    utilize gatsby-ssr.js to move the style tag generated by typography.js to the top of the document head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+// Move Typography.js styles to the top of the head section so they're
+// loaded first and can more easily be overridden elsewhere without the
+// use of !important.
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
+  headComponents.sort((x, y) => {
+    if (x.key === 'TypographyStyle') {
+      return -1
+    } else if (y.key === 'TypographyStyle') {
+      return 1
+    }
+    return 0
+  })
+  replaceHeadComponents(headComponents)
+}
