35072ddbdd685e78cbd6feaf4ba0c1e006200356
John C. Munson <johncmunson91@gmail.com>
2018-10-10 12:08:05 -0500

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
+import './404.css'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="glitch">
+      <h1>404</h1>
+      <p>
+        You just hit a route that doesn&#39;t exist... you should probably go
+        home now.
+      </p>
+    </div>
   </Layout>
 )
 
