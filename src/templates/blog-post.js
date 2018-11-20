ece7b0a920e315d18e2f1b1432a85e459bc50f51
John C. Munson <johncmunson91@gmail.com>
2018-11-20 16:30:09 -0600

    everything working great, but small bug when you click on a nav link. the bar seems to jump around a lot. need to disable the observer during scroll animation.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createElement } from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Link from '../components/link'
@@ -11,16 +11,19 @@ import slugify from '@sindresorhus/slugify'
 import StickyBox from 'react-sticky-box'
 import './blog-post.css'
 import { Link as ScrollLink } from 'react-scroll'
+import Observer from '@researchgate/react-intersection-observer'
+import rehypeReact from 'rehype-react'
 
 const NavLink = styled(ScrollLink)`
   box-shadow: none;
   text-decoration: none;
   color: inherit;
   display: block;
-  margin-left: ${props => (props.depth - 1) * 0.5 + 'em'};
+  padding-left: ${props => (props.depth - 1) * 0.5 + 'em'};
   opacity: 0.75;
   :hover {
     opacity: 1;
+    cursor: pointer;
   }
 `
 
@@ -42,133 +45,286 @@ const generateHeadingNumbers = headings => {
   })
 }
 
-class NavHeadings extends Component {
+// class NavHeadings extends Component {
+//   state = {
+//     activeNavHeading: null
+//   }
+//   handleSetActive = e => {
+//     console.log(e)
+//     this.setState({ activeNavHeading: e })
+//     history.replaceState(undefined, undefined, `#${e}`)
+//   }
+//   handleNavHeadingClick = heading => {
+//     this.handleSetActive(heading)
+//   }
+//   render() {
+//     return this.props.headings.map((heading, i) => (
+//       <NavLink
+//         className={
+//           this.state.activeNavHeading === slugify(heading.value) &&
+//           'active-nav-link'
+//         }
+//         key={i}
+//         depth={heading.depth}
+//         to={`${slugify(heading.value)}`}
+//         smooth={true}
+//         offset={-80}
+//         onSetActive={this.handleSetActive}
+//         onClick={() => this.handleNavHeadingClick(slugify(heading.value))}
+//       >
+//         <Text size="0.7em" style={{ lineHeight: 1.2, marginBottom: '0.5em' }}>
+//           <span style={{ marginRight: '0.25em' }}>{heading.tocNumber}.</span>
+//           <span>{heading.value}</span>
+//         </Text>
+//       </NavLink>
+//     ))
+//   }
+// }
+
+const NavHeadings = ({ headings, activeNavHeading, handleNavHeadingClick }) =>
+  headings.map((heading, i) => (
+    <NavLink
+      key={i}
+      className={
+        activeNavHeading === slugify(heading.value) && 'active-nav-link'
+      }
+      depth={heading.depth}
+      to={`${slugify(heading.value)}`}
+      smooth={true}
+      offset={-80}
+      onClick={() => handleNavHeadingClick(slugify(heading.value))}
+    >
+      <Text size="0.7em" style={{ lineHeight: 1.2, marginBottom: '0.5em' }}>
+        <span style={{ marginRight: '0.3em' }} />
+        <span>{heading.value}</span>
+      </Text>
+    </NavLink>
+  ))
+
+const getObservedHeading = (el, onChange) => ({ id, children }) => (
+  <Observer onChange={e => onChange(e, id)} rootMargin="0% 0% -85%">
+    {createElement(el, { id: id }, children)}
+  </Observer>
+)
+
+const getRenderAst = components =>
+  new rehypeReact({
+    createElement: createElement,
+    components: components
+  }).Compiler
+
+class Template extends Component {
   state = {
-    activeNavHeading: null
+    activeNavHeading: null,
+    handleObserverChangeAttempts: 0
   }
-  handleSetActive = e => {
-    console.log(e)
-    this.setState({ activeNavHeading: e })
-    history.replaceState(undefined, undefined, `#${e}`)
+  handleObserverChange = (e, heading) => {
+    this.setState({
+      ...this.state,
+      handleObserverChangeAttempts: this.state.handleObserverChangeAttempts + 1
+    })
+    if (
+      this.state.handleObserverChangeAttempts >
+      this.props.data.markdownRemark.headings.length
+    )
+      this.handleNavHeadingClick(heading)
   }
   handleNavHeadingClick = heading => {
-    this.handleSetActive(heading)
+    this.setState({ ...this.state, activeNavHeading: heading })
+    history.replaceState(undefined, undefined, `#${heading}`)
+  }
+  markdownToComponentMap = {
+    h1: getObservedHeading('h1', this.handleObserverChange),
+    h2: getObservedHeading('h2', this.handleObserverChange),
+    h3: getObservedHeading('h3', this.handleObserverChange),
+    h4: getObservedHeading('h4', this.handleObserverChange),
+    h5: getObservedHeading('h5', this.handleObserverChange),
+    h6: getObservedHeading('h6', this.handleObserverChange)
   }
   render() {
-    return this.props.headings.map((heading, i) => (
-      <NavLink
-        className={
-          this.state.activeNavHeading === slugify(heading.value) &&
-          'active-nav-link'
-        }
-        key={i}
-        depth={heading.depth}
-        to={`${slugify(heading.value)}`}
-        spy={true}
-        smooth={true}
-        offset={-80}
-        onSetActive={this.handleSetActive}
-        onClick={() => this.handleNavHeadingClick(slugify(heading.value))}
+    const { markdownRemark: post, site } = this.props.data
+    const { frontmatter, html, htmlAst, headings } = post
+    const { title, date, excerpt, path } = frontmatter
+    const { next, prev } = this.props.pageContext
+    if (headings.length) generateHeadingNumbers(headings)
+    return (
+      <Layout
+        location={location}
+        renderNav={() => {
+          if (headings.length) {
+            return (
+              <StickyBox
+                offsetTop={5}
+                style={{
+                  marginLeft: '2em',
+                  marginTop: '11.25em',
+                  lineHeight: rhythm(1)
+                }}
+              >
+                <Text
+                  size="0.85em"
+                  style={{ opacity: 0.75, marginBottom: '0.5em' }}
+                >
+                  Contents
+                </Text>
+                <NavHeadings
+                  headings={headings}
+                  activeNavHeading={this.state.activeNavHeading}
+                  handleNavHeadingClick={this.handleNavHeadingClick}
+                />
+              </StickyBox>
+            )
+          }
+        }}
       >
-        <Text size="0.7em" style={{ lineHeight: 1.2, marginBottom: '0.5em' }}>
-          <span style={{ marginRight: '0.25em' }}>{heading.tocNumber}.</span>
-          <span>{heading.value}</span>
-        </Text>
-      </NavLink>
-    ))
+        <Helmet
+          title={`${title} | ${site.siteMetadata.title}`}
+          meta={[{ name: 'description', content: excerpt }]}
+          htmlAttributes={{ lang: 'en' }}
+        />
+        <div>
+          <div style={{ marginBottom: '0.2em', ...scale(0.5) }}>
+            <i>
+              <b>{title}</b>
+            </i>
+          </div>
+          <Text size="0.8em" style={{ marginBottom: '3.5em' }}>
+            <i>{date}</i>
+          </Text>
+          <div style={{ marginBottom: rhythm(2) }}>
+            {getRenderAst(this.markdownToComponentMap)(htmlAst)}
+          </div>
+          <Bio />
+          <hr />
+          <ul
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+              listStyle: 'none',
+              padding: 0,
+              margin: 0
+            }}
+          >
+            <li>
+              {prev && (
+                <Link to={prev.frontmatter.path} rel="prev">
+                  <small style={{ letterSpacing: 1.2 }}>
+                    <i>
+                      <b>← Previous</b>
+                    </i>
+                  </small>
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.frontmatter.path} rel="next">
+                  <small style={{ letterSpacing: 1.2 }}>
+                    <i>
+                      <b>Next →</b>
+                    </i>
+                  </small>
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      </Layout>
+    )
   }
 }
 
-const Template = ({ data, location, pageContext }) => {
-  const { markdownRemark: post, site } = data
-  const { frontmatter, html, headings } = post
-  const { title, date, excerpt, path } = frontmatter
-  const { next, prev } = pageContext
-  if (headings.length) generateHeadingNumbers(headings)
-  return (
-    <Layout
-      location={location}
-      renderNav={() => {
-        if (headings.length) {
-          return (
-            <StickyBox
-              offsetTop={5}
-              style={{
-                marginLeft: '2em',
-                marginTop: '11.25em',
-                lineHeight: rhythm(1),
-                fontStyle: 'italic'
-              }}
-            >
-              <Text
-                size="0.85em"
-                style={{ opacity: 0.75, marginBottom: '0.5em' }}
-              >
-                Outline:
-              </Text>
-              <NavHeadings headings={headings} />
-            </StickyBox>
-          )
-        }
-      }}
-    >
-      <Helmet
-        title={`${title} | ${site.siteMetadata.title}`}
-        meta={[{ name: 'description', content: excerpt }]}
-        htmlAttributes={{ lang: 'en' }}
-      />
-      <div>
-        <div style={{ marginBottom: '0.2em', ...scale(0.5) }}>
-          <i>
-            <b>{title}</b>
-          </i>
-        </div>
-        <Text size="0.8em" style={{ marginBottom: '3.5em' }}>
-          <i>{date}</i>
-        </Text>
-        <div
-          style={{ marginBottom: rhythm(2) }}
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-        <Bio />
-        <hr />
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-            margin: 0
-          }}
-        >
-          <li>
-            {prev && (
-              <Link to={prev.frontmatter.path} rel="prev">
-                <small style={{ letterSpacing: 1.2 }}>
-                  <i>
-                    <b>← Previous</b>
-                  </i>
-                </small>
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.frontmatter.path} rel="next">
-                <small style={{ letterSpacing: 1.2 }}>
-                  <i>
-                    <b>Next →</b>
-                  </i>
-                </small>
-              </Link>
-            )}
-          </li>
-        </ul>
-      </div>
-    </Layout>
-  )
-}
+// const Template = ({ data, location, pageContext }) => {
+//   const { markdownRemark: post, site } = data
+//   const { frontmatter, html, headings } = post
+//   const { title, date, excerpt, path } = frontmatter
+//   const { next, prev } = pageContext
+//   if (headings.length) generateHeadingNumbers(headings)
+//   return (
+//     <Layout
+//       location={location}
+//       renderNav={() => {
+//         if (headings.length) {
+//           return (
+//             <StickyBox
+//               offsetTop={5}
+//               style={{
+//                 marginLeft: '2em',
+//                 marginTop: '11.25em',
+//                 lineHeight: rhythm(1),
+//                 fontStyle: 'italic'
+//               }}
+//             >
+//               <Text
+//                 size="0.85em"
+//                 style={{ opacity: 0.75, marginBottom: '0.5em' }}
+//               >
+//                 Outline:
+//               </Text>
+//               <NavHeadings headings={headings} />
+//             </StickyBox>
+//           )
+//         }
+//       }}
+//     >
+//       <Helmet
+//         title={`${title} | ${site.siteMetadata.title}`}
+//         meta={[{ name: 'description', content: excerpt }]}
+//         htmlAttributes={{ lang: 'en' }}
+//       />
+//       <div>
+//         <div style={{ marginBottom: '0.2em', ...scale(0.5) }}>
+//           <i>
+//             <b>{title}</b>
+//           </i>
+//         </div>
+//         <Text size="0.8em" style={{ marginBottom: '3.5em' }}>
+//           <i>{date}</i>
+//         </Text>
+//         <div
+//           style={{ marginBottom: rhythm(2) }}
+//           dangerouslySetInnerHTML={{ __html: html }}
+//         />
+//         <Bio />
+//         <hr />
+//         <ul
+//           style={{
+//             display: 'flex',
+//             flexWrap: 'wrap',
+//             justifyContent: 'space-between',
+//             listStyle: 'none',
+//             padding: 0,
+//             margin: 0
+//           }}
+//         >
+//           <li>
+//             {prev && (
+//               <Link to={prev.frontmatter.path} rel="prev">
+//                 <small style={{ letterSpacing: 1.2 }}>
+//                   <i>
+//                     <b>← Previous</b>
+//                   </i>
+//                 </small>
+//               </Link>
+//             )}
+//           </li>
+//           <li>
+//             {next && (
+//               <Link to={next.frontmatter.path} rel="next">
+//                 <small style={{ letterSpacing: 1.2 }}>
+//                   <i>
+//                     <b>Next →</b>
+//                   </i>
+//                 </small>
+//               </Link>
+//             )}
+//           </li>
+//         </ul>
+//       </div>
+//     </Layout>
+//   )
+// }
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
@@ -179,6 +335,7 @@ export const pageQuery = graphql`
     }
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
+      htmlAst
       headings {
         value
         depth
