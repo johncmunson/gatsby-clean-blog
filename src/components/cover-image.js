013f53c9273a51cf938d3e9d19fee6e231c4550d
John C. Munson <johncmunson91@gmail.com>
2018-11-29 23:52:21 -0600

    add background highlights to the coverpage text. also, add ability to turn off coverpages on the blog posts.

--- src/components/cover-image.js
+++ src/components/cover-image.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Img from 'gatsby-image'
 import Text from './text'
 import Link from './link'
+import GhAnchor from './gh-anchor'
 import styled from 'styled-components'
 import { rhythm, scale } from '../utils/typography'
 
@@ -21,10 +22,26 @@ export default ({ img, title, date, to }) => {
   const buildCoverTitle = () => {
     if (to) {
       return (
-        <div className="post-title" style={{ ...scale(0.5) }}>
+        <div
+          className="post-title"
+          style={{
+            ...scale(0.5),
+            background: 'rgba(0, 0, 0, 0.5)',
+            transform: 'skew(-10deg)'
+          }}
+        >
           <i>
             <b>
-              <Link to={to}>{title}</Link>
+              <Link
+                to={to}
+                style={{
+                  padding: '0 0.25rem',
+                  transform: 'skew(10deg)',
+                  display: 'inline-block'
+                }}
+              >
+                <span>{title}</span>
+              </Link>
             </b>
           </i>
         </div>
@@ -32,22 +49,20 @@ export default ({ img, title, date, to }) => {
     }
     return (
       <div className="post-title" style={{ ...scale(0.5) }}>
-        <a href="#" aria-hidden="true" className="anchor">
-          <svg
-            aria-hidden="true"
-            height="16"
-            version="1.1"
-            viewBox="0 0 16 16"
-            width="16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-            />
-          </svg>
-        </a>
+        <GhAnchor />
         <i>
-          <b>{title}</b>
+          <b
+            style={{
+              background: 'rgba(0, 0, 0, 0.5)',
+              padding: '0 0.25rem',
+              transform: 'skew(-10deg)',
+              display: 'inline-block'
+            }}
+          >
+            <span style={{ transform: 'skew(10deg)', display: 'inline-block' }}>
+              {title}
+            </span>
+          </b>
         </i>
       </div>
     )
@@ -56,8 +71,19 @@ export default ({ img, title, date, to }) => {
     <CoverWrapper to={to}>
       <Img fluid={img} style={{ borderRadius: '0.2em' }} />
       <CoverText>
-        <Text size="0.8em">
-          <i>{date}</i>
+        <Text
+          size="0.8em"
+          style={{
+            background: 'rgba(0, 0, 0, 0.5)',
+            transform: 'skew(-10deg)',
+            padding: '0 0.25rem',
+            marginBottom: '0.2em',
+            display: 'inline-block'
+          }}
+        >
+          <i style={{ transform: 'skew(10deg)', display: 'inline-block' }}>
+            {date}
+          </i>
         </Text>
         {buildCoverTitle()}
       </CoverText>
