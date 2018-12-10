a187b8fdd0c3969c6fd97d47754874503a563a08
John C. Munson <johncmunson91@gmail.com>
2018-12-10 14:43:59 -0600

    add clarifying comments

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,6 +9,10 @@
 const ReactGA = require('react-ga')
 ReactGA.initialize('UA-130692445-1')
 
+// There is also a gatsby-plugin for google analytics that could be used.
+// Find instructions for setting it up in the official Gatsby docs.
+// https://www.gatsbyjs.org/docs/adding-analytics/#adding-analytics
+
 exports.onRouteUpdate = (state, page, pages) => {
   if (process.env.NODE_ENV === 'production') {
     ReactGA.pageview(state.location.pathname)
