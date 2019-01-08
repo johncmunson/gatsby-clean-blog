cf48f13c9eced3d9be75e19da10d1ccb1199675a
John C. Munson <johncmunson91@gmail.com>
2019-01-08 11:22:03 -0600

    integrate lodash into the project

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,14 @@
-function compose() {
-  var fns = arguments
-  return function(result) {
-    for (var i = fns.length - 1; i > -1; i--) {
-      result = fns[i].call(this, result)
-    }
-    return result
-  }
-}
+import { compose } from 'lodash/fp'
+
+// function compose() {
+//   var fns = arguments
+//   return function(result) {
+//     for (var i = fns.length - 1; i > -1; i--) {
+//       result = fns[i].call(this, result)
+//     }
+//     return result
+//   }
+// }
 
 export const disableScrollAtTopAndBottom = (e, target) => {
   const { deltaY, deltaX } = e
